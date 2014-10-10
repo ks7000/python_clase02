702c850f3edb099b5b432ff5453ba53e5dc48265
ks7000 <jimmyolano@hotmail.com>
2014-10-10 16:33:07 -0430

    Ahora practicamos la herencia

---
+++ herencia.py
@@ -0,0 +1,13 @@
+from clases import Auto
+
+class Helicoptero(Auto):
+    pass
+mi_helicoptero = Helicoptero(3)
+mi_helicoptero.arrancar()
+mi_helicoptero.conducir()
+mi_helicoptero.arrancar()
+mi_helicoptero.conducir()
+mi_helicoptero.arrancar()
+mi_helicoptero.conducir()
+mi_helicoptero.arrancar()
+mi_helicoptero.conducir()
