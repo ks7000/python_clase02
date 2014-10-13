a89f7b4845316ca1418d92f71a83a51a99643550
ks7000 <jimmyolano@hotmail.com>
2014-10-13 15:04:03 -0430

    Decoradores

--- clases.py
+++ clases.py
@@ -3,13 +3,20 @@ class Auto:
     def __init__(self, gasolina):
         self.gasolina = gasolina
     
+    def decorar(f):
+        def inner(*args, **kwargs):
+            f(*args, **kwargs)
+            print "Se ha ejecutado %s" % f.__name__
+        return inner
+    
+    @decorar
     def arrancar(self):
         if self.gasolina > 0:
             print "Arranca"
         else:
             print "NO arranca"
     
-    
+    @decorar
     def conducir(self):
         if self.gasolina > 0:
             self.gasolina -=1
@@ -17,3 +24,4 @@ class Auto:
         else:
             print "No se mueve"
 
+mi_decorador = decorar(arrancar())
