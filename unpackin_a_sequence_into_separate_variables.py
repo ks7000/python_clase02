1068a3cb775ea999db58a3d2b882d75651194929
ks7000 <jimmyolano@hotmail.com>
2014-10-13 15:28:14 -0430

    Python Cookbook 1.1

---
+++ unpackin_a_sequence_into_separate_variables.py
@@ -0,0 +1,21 @@
+p = (4, 5)
+x, y = p
+print str(x)
+print str(y)
+data = [ 'ACME', 50, 91.1, (2012, 12, 21)]
+name, shares, price, date = data
+print name
+print date
+name, shares, price, (year, mon, day) =data
+print name
+print year
+print mon
+print day
+s = 'Hello'
+a, b, c, d, e = s
+print a
+print b
+print e
+_, segundo, tercero, _ = data
+print segundo
+print tercero
