74795acd9ca510aa8258f9a79341ce5af5f01981
Jimmy_Olano <olano@ks7000.net.ve>
2014-10-17 10:23:43 -0430

    Agrego 'click.pocoo.org' al archivo 'funciones.py'

--- funcion.py
+++ funcion.py
@@ -1,7 +1,14 @@
+# -*- encoding=utf-8 -*-
+import click
+
+@click.command()
+@click.option('--nombre', default=u'Hugo', prompt=u'Introduzca su nombre, por favor', help='Si usted introduce su nombre lo imprimimos por pantalla.')
+@click.option('--apellido', default=u'Chávez', prompt=u'Introduzca su apellido, por favor', help='Si usted introduce su apellido lo imprimimos por pantalla.')
 def nombres(nombre, apellido):
     nombre_completo = nombre , apellido
-    print "Mi nombres es %s %s" ( nombre , apellido)
+    print "Mi nombre completo es ", nombre , " ", apellido
     return nombre_completo
 
-print nombres("jimmy", "olano")
+if __name__ == '__main__':
+    nombres()
 
